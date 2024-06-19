975f85b0695aa7fd46aa071c77d722ed90fd11a2
FelipeDieguez <felipedieguez2000@hotmail.com>
2024-06-19 19:50:04 -0300

    fix descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
   for (let i = 0; i < filterItems.length; i++) {
-    if (selectedValue === "all") {
+    if (selectedValue === "todos") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
       filterItems[i].classList.add("active");
